9f12a5821f0fa2f281db2eee7a3497ea86d23112
NicolasNdour <nicolasmndour@hotmail.com>
2019-04-17 01:51:41 +0200

    quatrieme commit

---
+++ app/src/main/java/com/drawer/myapplication/Hommes.java
@@ -0,0 +1,13 @@
+package com.drawer.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hommes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hommes);
+    }
+}
--- app/src/main/java/com/drawer/myapplication/MainActivity.java
+++ app/src/main/java/com/drawer/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.drawer.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,10 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    Button btnHommes,btnFemmes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +42,24 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        btnHommes = (Button)findViewById(R.id.btnHommes);
+        btnFemmes = (Button)findViewById(R.id.btnFemmes);
+
+        btnHommes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Hommes.class);
+                startActivity(intent);
+            }
+        });
+
+        btnFemmes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Femmes.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
